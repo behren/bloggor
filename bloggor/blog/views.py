883de0d2cb1a01e4eddc19bb5d495648b6bf6198
Niclas Behrendt <niclas.behrendt@msg.group>
2021-03-09 09:46:22 +0100

    improved templates, added static/css

--- bloggor/blog/views.py
+++ bloggor/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        "author": "behren",
+        "title": "Post 1",
+        "content": "Liste der VHV-Stages",
+        "date_posted": "9. März 2021",
+    },
+    {
+        "author": "schadb1",
+        "title": "Post 2",
+        "content": "Liste der BVA-Stages",
+        "date_posted": "10. März 2021",
+    },
+]
+
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/stages.html", {"title": "Stages"})
